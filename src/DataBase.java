f6010eb94f8472d5b328ead5dfbd83eeb497940d
po-wei <b02502129@ntu.edu.tw>
2017-06-09 22:03:40 +0800

    ver0

---
+++ src/DataBase.java
@@ -0,0 +1,22 @@
+
+public class DataBase
+{
+	public static final String DBtype = "MongoDB";
+	
+	DataBase()
+	{
+		// TODO
+		// dump data
+	}
+	
+	public Object queryByID(int ID)
+	{
+		return o];
+	}
+	
+	
+	
+}
+
+
+
---
+++ src/TicketSystem.java
@@ -0,0 +1,15 @@
+
+public class TicketSystem
+{
+	private Inquirier myInq;
+	private Refunder myRefunder;
+	private Booker myBooker;
+	
+	TicketSystem()
+	{
+		myInq = new Inquirier();
+		myRefunder = new Refunder();
+		myBooker = new Booker();
+	}
+
+}
