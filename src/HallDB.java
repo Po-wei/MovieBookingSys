75f99a80c0b22142992fb99fb6395a0792e1dd47
po-wei <b02502129@ntu.edu.tw>
2017-06-12 01:35:56 +0800

    MongoDB

--- src/HallDB.java
+++ src/HallDB.java
@@ -1,12 +1,24 @@
+import org.bson.Document;
+
+import com.mongodb.*;
+import com.mongodb.client.*;
 
 public class HallDB implements Database
 {
-
 	@Override
 	public Hall queryByID(String ID)
 	{
 		// TODO Auto-generated method stub
-		return super.queryByID(ID);
+		return null;
+	}
+	
+	
+	public static void Main(String[] args)
+	{
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase database = mongoClient.getDatabase("TheDatabaseName");
+		MongoCollection<Document> collection = database.getCollection("TheCollectionName");
+		
 	}
 
 }
