e1921a52812c4240a00dc6abf599d77d452273cb
James Huang <jameshuang304@gmail.com>
2017-06-13 21:09:22 +0800

    add case SMALL_HALL in hallInit()

--- src/HallDB.java
+++ src/HallDB.java
@@ -73,28 +73,42 @@ public class HallDB implements Database
 	{
 		MongoClient mongoClient = new MongoClient();
 		this.database = mongoClient.getDatabase("TicketSys");
-		this.bigRoom = database.getCollection("big_room");
-		this.smallRoom = database.getCollection("small_room");
+		//this.bigRoom = database.getCollection("big_room");
+		//this.smallRoom = database.getCollection("small_room");
 	}
 	
 	private void hallInit(MongoCollection<Document> myCollection, HallType hallType)
 	{
+		FindIterable<Document> findIterable;
+		MongoCursor<Document> mongoCursor;
 		switch(hallType)
 		{
-		case BIG_HALL:
-			FindIterable<Document> findIterable = database.getCollection("big_room").find();
-			MongoCursor<Document> mongoCursor = findIterable.iterator();
-//			while (mongoCursor.hasNext()) {
-//				Document doc = mongoCursor.next();
-//				System.out.println(doc);
-//				myCollection.insertOne(doc);
-//
-//				//System.out.println(doc.getString("row") + doc.getInteger("seatNum"));
-//
-//			}
-			myCollection.insertOne(new Document("GG","VERY BIG"));
-			break;
-			
+			case BIG_HALL:
+				findIterable = database.getCollection("big_room").find();
+				mongoCursor = findIterable.iterator();
+				while (mongoCursor.hasNext()) {
+					Document doc = mongoCursor.next();
+					//System.out.println(doc);
+					//myCollection.insertOne(doc);
+
+					//System.out.println(doc.getString("row") + doc.getInteger("seatNum"));
+				myCollection.insertOne(new Document("GG","VERY BIG"));
+				}
+				//myCollection.insertOne(new Document("GG","VERY BIG"));
+				break;
+
+			case SMALL_HALL:
+				findIterable = database.getCollection("small_room").find();
+				mongoCursor = findIterable.iterator();
+				while (mongoCursor.hasNext()) {
+					Document doc = mongoCursor.next();
+					myCollection.insertOne(doc);
+				}
+				break;
+
+			default:
+				break;
+
 		}
 	}
 
@@ -131,7 +145,7 @@ public class HallDB implements Database
 
 		HallDB db = new HallDB();
 		//db.test();
-		db.createHall("asd", "9:30", HallType.BIG_HALL);
+		db.createHall("asd", "9:30", HallType.SMALL_HALL);
 
 	}
 
