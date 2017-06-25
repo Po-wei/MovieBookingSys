c76cb592bb01c4b97e4900577c080f4e28405d21
po-wei <b02502129@ntu.edu.tw>
2017-06-26 01:38:52 +0800

    merge try part 2

--- src/Refunder.java
+++ src/Refunder.java
@@ -30,13 +30,118 @@ public class Refunder
         DateFormat df = new SimpleDateFormat("HH:mm");
         Date now = new Date();
         Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
+    public String refund(String ticketID){
+        DateFormat df = new SimpleDateFormat("HH:mm");
+        Date now = new Date();
+        Ticket ticket = ticketDB.queryByID(ticketID);
         String message = "";
         if(null == ticket) message = "退票失敗，此電影票ID不存在";
         else if(t2ms(ticket.startTime) - t2ms(df.format(now)) > 1200000) {
             message = "退票成功，全額退款";
             //ticket.available();  //待實作
             ticketDB.available(ticket);
+            //ticket.available();  //待實作
+            ticketDB.available(ticket);
             hallDB.cancelSeat(ticket);
+        }
+        else message = "退票失敗，退票需於開場時間前20分鐘前";
+        //System.out.println(message);
+
+        String alert = message;
+        return alert;
+    }
+
+    private long t2ms(String time){
+        String fuck = time.replace("：",":");
+        SimpleDateFormat f = new SimpleDateFormat("HH:mm");
+        Date d = null;
+        try {
+            d = f.parse(fuck);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+        return d.getTime();
+    }
+}
+            //ticket.available();  //待實作
+            ticketDB.available(ticket);
+            hallDB.cancelSeat(ticket);
+        }
+        else message = "退票失敗，退票需於開場時間前20分鐘前";
+        //System.out.println(message);
+
+        String alert = message;
+        return alert;
+    }
+
+    private long t2ms(String time){
+        String fuck = time.replace("：",":");
+        SimpleDateFormat f = new SimpleDateFormat("HH:mm");
+        Date d = null;
+        try {
+            d = f.parse(fuck);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+        return d.getTime();
+    }
+}            hallDB.cancelSeat(ticket);
         }
         else message = "退票失敗，退票需於開場時間前20分鐘前";
         //System.out.println(message);
